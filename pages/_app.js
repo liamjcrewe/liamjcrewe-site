5f3533e9ab8499a83cc96f46ac501820ea519d5c
Liam Crewe <liam.crewe@mayden.co.uk>
2019-10-25 12:59:07 +0100

    Remove container as not needed in nextjs v9

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import 'sanitize.css/forms.css'
 import 'sanitize.css/typography.css'
 
 import React from 'react'
-import App, { Container } from 'next/app'
+import App from 'next/app'
 
 import { ThemeProvider } from 'styled-components'
 
@@ -30,11 +30,9 @@ export default class ThemedApp extends App {
     const { Component, pageProps } = this.props
 
     return (
-      <Container>
-        <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     )
   }
 }
