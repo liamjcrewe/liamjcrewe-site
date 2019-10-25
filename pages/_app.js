8b853c902bd73d02f8b5643566d8c108bf739a6c
Liam Crewe <liam.crewe@mayden.co.uk>
2019-10-25 19:44:15 +0100

    Remove getInitialProps from _app
    This doesn't do anything and just slows down the app.

--- pages/_app.js
+++ pages/_app.js
@@ -17,16 +17,6 @@ const theme = {
 }
 
 export default class ThemedApp extends App {
-  static async getInitialProps ({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render () {
     const { Component, pageProps } = this.props
 
