42ed6a2456907d3a8afc6fea45326e403c4ce705
Liam Crewe <liam.crewe@mayden.co.uk>
2019-10-26 02:06:21 +0100

    Update collectCoverageFrom and add comments

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,15 @@ module.exports = {
   collectCoverageFrom: [
     '*.js',
     '**/*.js',
+    // ignore jest lib files
     '!**/coverage/**/*.js',
+    '!jest.config.js',
+    // ignore nextjs lib files
     '!out/**/*.js',
     '!next.config.js',
-    '!jest.config.js',
-    '!webpack.config.js'
+    // ignore nextjs magic pages
+    // these are copied almost exactly from official nextjs docs and examples
+    '!pages/_app.js',
+    '!pages/_document.js'
   ]
 }
