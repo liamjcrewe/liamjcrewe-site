dd6794bef12dca5c2e54e5b6eda7e83a1ed3c45c
Liam Crewe <liam.crewe@mayden.co.uk>
2019-10-26 02:06:34 +0100

    Add coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   testPathIgnorePatterns: ['/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
   collectCoverageFrom: [
     '*.js',
     '**/*.js',
